3e0c30205e2e899410cf0a527e10299de16fffe1
Terry <terrybd@gmail.com>
2021-03-13 17:00:21 -0500

    Trying to introduce HotChocolate with Mongo
    Adding GraphQL, but stuck on mongo ObjectId serialize/deserialize

---
+++ src/graph/Query.Pizza.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using graph.Schema;
+using HotChocolate;
+using MongoDB.Driver;
+
+namespace graph
+{
+    public partial class Query
+    {
+        public IQueryable<Pizza> GetPizza([Service] IMongoDatabase database) =>
+            database.GetCollection<Pizza>("pizzas").AsQueryable();
+    }
+}
---
+++ src/graph/Schema/Pizza.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace graph.Schema
+{
+    public class Pizza
+    {
+        [BsonId] public ObjectId Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public IEnumerable<Ingredient> Ingredients { get; set; }
+    }
+
+    public class Ingredient
+    {
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+    }
+
+}
